e0102b8d68c6f64f29fba83250d554a4352818e0
kinnch <kinnch@gmail.com>
2019-01-13 13:44:52 +0700

    connect with line bot

--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/kinnch/line-todo/servicemanagement"
+	"github.com/kinnch/line-todo/servicemanagement/delivery/http"
+
 	_ "github.com/heroku/x/hmetrics/onload"
-	"github.com/russross/blackfriday"
+	"github.com/labstack/echo"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
+
+func connectLineBot() *linebot.Client {
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return bot
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -17,18 +30,22 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	// router := gin.New()
+	// router.Use(gin.Logger())
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	// })
 
-	router.GET("/mark2", func(c *gin.Context) {
-		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!5555**"))))
-	})
+	// router.GET("/mark2", func(c *gin.Context) {
+	// 	c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!5555**"))))
+	// })
 
-	router.Run(":" + port)
+	// router.Run(":" + port)
+	e := echo.New()
+	bankCoreInfo := servicemanagement.NewBankCoreServiceInfo()
+	http.NewServiceHTTPHandler(e, connectLineBot(), bankCoreInfo)
+	e.Logger.Fatal(e.Start(":" + port))
 }
