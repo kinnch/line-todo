7dc42dac37f9b1bdb00c61f305a504e9c7205290
kinnch <kinnch@gmail.com>
2019-01-13 18:11:38 +0700

    add function getDateTimeFromString to convert input to object time.Time

--- core/todo/todo.go
+++ core/todo/todo.go
@@ -4,19 +4,67 @@ import (
 	"errors"
 	"fmt"
 	"github.com/kinnch/line-todo/models"
+	"github.com/kinnch/line-todo/utilities/sysVar"
 	"strings"
+	"time"
 )
 
-
-
-func TodoController(user_id string ,message string) string {
+func TodoController(user_id string, message string) string {
 	return fmt.Sprintf("we recieve your message: \"%s\" from user: %s \n", message, user_id)
 }
-func decodeMessage(message string) (models.Todo , error) {
+func decodeMessage(user_id string, message string) (models.Todo, error) {
+	var todo models.Todo
 	splted := strings.Split(message, ":")
 	// we default time if not specified
 	if len(splted) < 2 {
-		return models.Todo{}, errors.New("invalid format message")
+		return todo, errors.New("invalid format message")
 	}
-	return models.Todo{},nil
+	if splted[0] == "" {
+		return todo, errors.New("task provided is invalid")
+	}
+	dtm, err := getDateTimeFromString(splted[1], splted[2])
+	if err != nil {
+		return todo, err
+	}
+	todo = models.Todo{
+		Task:   splted[0],
+		Time:   dtm,
+		UserID: user_id,
+	}
+	return todo, nil
+}
+
+func getDateTimeFromString(dateStr string, timeStr string) (time.Time, error) {
+	var date string
+	var t string
+	if dateStr == "" {
+		return time.Time{}, errors.New("date is required")
+	} else if strings.ToUpper(dateStr) == "TODAY" {
+		date = time.Now().In(sysVar.Location()).Format("2006-01-02")
+	} else if strings.ToUpper(dateStr) == "TOMORROW" {
+		date = time.Now().AddDate(0, 0, 1).In(sysVar.Location()).Format("2006-01-02")
+	} else {
+		d, err := time.ParseInLocation("2/1/06", dateStr, sysVar.Location())
+		if err != nil {
+			return time.Time{}, err
+		}
+		date = d.Format("2006-01-02")
+	}
+
+	if timeStr == "" {
+		t = time.Now().In(sysVar.Location()).Format("15:04:05Z07:00")
+	} else {
+		tmp, err := time.ParseInLocation("15:04", timeStr, sysVar.Location())
+		if err != nil {
+			return time.Time{}, err
+		}
+		t = tmp.Format("15:04:05+07:00")
+	}
+
+	dtm, err := time.Parse(time.RFC3339, date+"T"+t)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return dtm, nil
+
 }
--- core/todo/todo_test.go
+++ core/todo/todo_test.go
@@ -3,10 +3,51 @@ package todo
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func Test_decodeMessage_invalid_format(t *testing.T) {
-	todo,err := decodeMessage("Buy milk")
-	assert.Zero(t,todo)
-	assert.NotNil(t,err)
+	todo, err := decodeMessage("Buy milk")
+	assert.Zero(t, todo)
+	assert.NotNil(t, err)
+}
+
+func Test_getDateTimeFromString_convert_dateStr__success(t *testing.T) {
+	dtm, err := getDateTimeFromString("TodAy", "")
+	assert.Nil(t, err)
+	duration, _ := time.ParseDuration("1s")
+	assert.WithinDuration(t, time.Now(), dtm, duration)
+
+	dtm, err = getDateTimeFromString("Tomorrow", "")
+	assert.Nil(t, err)
+	assert.WithinDuration(t, time.Now().AddDate(0, 0, 1), dtm, duration)
+
+	dtm, err = getDateTimeFromString("2/5/18", "")
+	assert.Nil(t, err)
+	expDtm, err := time.Parse(time.RFC3339, "2018-05-02T"+time.Now().Format("15:04:05Z07:00"))
+	assert.Nil(t, err)
+	assert.WithinDuration(t, dtm, expDtm, duration)
+
+}
+
+func Test_getDateTimeFromString_convert_dateStr__fail(t *testing.T) {
+	_, err := getDateTimeFromString("", "")
+	assert.NotNil(t, err)
+
+	_, err = getDateTimeFromString("12/31/18", "")
+	assert.NotNil(t, err)
+}
+
+func Test_getDateTimeFromString_convert_timeStr_success(t *testing.T) {
+	dtm, err := getDateTimeFromString("2/5/18", "15:04")
+	assert.Nil(t, err)
+	duration, _ := time.ParseDuration("1s")
+	expDtm, err := time.Parse(time.RFC3339, "2018-05-02T15:04:00+07:00")
+	assert.Nil(t, err)
+	assert.WithinDuration(t, dtm, expDtm, duration)
+}
+
+func Test_getDateTimeFromString_convert_timeStr_fail(t *testing.T) {
+	_, err := getDateTimeFromString("2/5/18", "30:04")
+	assert.NotNil(t, err)
 }
